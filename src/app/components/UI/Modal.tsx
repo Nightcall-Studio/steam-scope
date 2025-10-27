848eb8694b5adfcdd06d433b4d58825027de424f
JaveHunt <huseynzadejavid20@gmail.com>
2025-10-27 15:30:43 +0400

    Implemented Modal

--- src/app/components/UI/Modal.tsx
+++ src/app/components/UI/Modal.tsx
@@ -3,20 +3,27 @@
 import Portal from "../common/Portal";
 
 interface Props {
+  isOpen: boolean;
   onClose: () => void;
   children: React.ReactNode;
 }
 
-export default function Modal(props: Props) {
-  const { onClose, children } = props;
+export default function Modal({ isOpen, onClose, children }: Props) {
+  if (!isOpen) return null;
 
   return (
     <Portal>
-      <div className="fixed inset-0 z-50 bg-black/50 flex items-center justify-center">
-        <div className="relative bg-white/10 backdrop-blur-lg border border-white/20 p-8 rounded-2xl shadow-xl w-[90%] max-w-md">
+      <div
+        className="fixed inset-0 z-50 bg-black/50 flex items-center justify-center"
+        onClick={onClose}
+      >
+        <div
+          className="w-[290px] relative bg-white/10 backdrop-blur-lg border border-white/20 p-8 rounded-2xl shadow-xl"
+          onClick={(e) => e.stopPropagation()}
+        >
           <button
             onClick={onClose}
-            className="absolute top-4 right-4 text-white text-xl hover:scale-110 transition"
+            className="absolute top-4 right-4 text-white text-xl hover:scale-110 transition cursor-pointer"
           >
             ✕
           </button>
--- src/app/components/layout/Header.tsx
+++ src/app/components/layout/Header.tsx
@@ -5,30 +5,55 @@ import { useState } from "react";
 import Modal from "../UI/Modal";
 import Link from "next/link";
 
+const NavLinks = ({ className = "" }) => (
+  <ul className={`flex flex-col gap-6 ${className}`}>
+    <li>
+      <Link href={"#"}>Home</Link>
+    </li>
+    <li>
+      <Link href={"#"}>About</Link>
+    </li>
+    <li>
+      <Link href={"#"}>FAQ</Link>
+    </li>
+  </ul>
+);
+
 const Header = () => {
   const [isOpen, setIsOpen] = useState(false);
-  return (
-    <>
-      <header className="flex justify-between items-center font-semibold text-[1.1rem] py-5">
-        <Link href={"/"}>
-          <Image
-            src="/images/logo.svg"
-            alt="Logo"
-            width={97}
-            height={97}
-            className="cursor-pointer"
-          />
-        </Link>
 
-        <nav>
-          <ul className="flex gap-8 cursor-pointer max-lg:hidden text-[18px] font-semibold">
-            <li>Home</li>
-            <li>About</li>
-            <li>FAQ</li>
-          </ul>
-        </nav>
-      </header>
-    </>
+  return (
+    <header className="flex justify-between items-center font-semibold text-[1.1rem] py-5">
+      <Link href={"/"}>
+        <Image
+          src="/images/logo.svg"
+          alt="Logo"
+          width={97}
+          height={97}
+          className="cursor-pointer"
+        />
+      </Link>
+
+      {/* Desktop Links */}
+      <nav className="hidden lg:flex gap-8 text-[18px] font-semibold cursor-pointer">
+        <NavLinks className="flex-row gap-8" />
+      </nav>
+
+      {/* Modal Open Btn */}
+      <button className="lg:hidden" onClick={() => setIsOpen(true)}>
+        <Image
+          src="/images/burger-menu-icon.svg"
+          alt="Menu"
+          width={24}
+          height={24}
+        />
+      </button>
+
+      {/* Mobile Modal Menu */}
+      <Modal isOpen={isOpen} onClose={() => setIsOpen(false)}>
+        <NavLinks className="justify-center items-center" />
+      </Modal>
+    </header>
   );
 };
 
