1ea95b81f9f24a3575782576157865488594b8de
JaveHunt <huseynzadejavid20@gmail.com>
2025-10-27 14:53:12 +0400

    Removed Sign in

--- src/app/components/layout/Header.tsx
+++ src/app/components/layout/Header.tsx
@@ -27,47 +27,6 @@ const Header = () => {
             <li>FAQ</li>
           </ul>
         </nav>
-        <div className="flex justify-center items-center gap-2.5 cursor-pointer">
-          <Image
-            src="/images/steam-icon.svg"
-            alt="Steam icon"
-            width={32}
-            height={32}
-            className="cursor-pointer hidden sm:block"
-          />
-          <span className="hidden sm:block">Sign In</span>
-          <Image
-            src="/images/burger-menu-icon.svg"
-            alt="Burger menu icon"
-            width={26}
-            height={26}
-            className="block sm:hidden cursor-pointer"
-            onClick={() => setIsOpen(true)}
-          />
-
-          {isOpen && (
-            <Modal onClose={() => setIsOpen(false)}>
-              <nav className="flex flex-col items-center gap-6 text-white">
-                <ul className="flex flex-col gap-6 text-[18px] font-semibold cursor-pointer">
-                  <li>Home</li>
-                  <li>About</li>
-                  <li>FAQ</li>
-                </ul>
-
-                <div className="flex justify-center items-center gap-2.5 cursor-pointer">
-                  <Image
-                    src="/images/steam-icon.svg"
-                    alt="Steam icon"
-                    width={32}
-                    height={32}
-                    className="cursor-pointer"
-                  />
-                  <span className="text-lg font-medium">Sign In</span>
-                </div>
-              </nav>
-            </Modal>
-          )}
-        </div>
       </header>
     </>
   );
